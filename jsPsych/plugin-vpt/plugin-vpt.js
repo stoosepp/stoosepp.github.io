bde9c6bdda1faadb1fc34a753f90115c30ce0215
Stoo Sepp <stoosepp@gmail.com>
2025-01-30 17:04:54 +1100

    updating Plugin for global var

--- jsPsych/plugin-vpt/plugin-vpt.js
+++ jsPsych/plugin-vpt/plugin-vpt.js
@@ -525,12 +525,12 @@ var jsPsychVPT = (function (jspsych) {
           console.log(
             `Running Grid with level ${current_grid.level} and trial ${current_grid.trial}`
           );
-          jsPsych.pluginAPI.setTimeout(function () {
+          this.jsPsych.pluginAPI.setTimeout(function () {
             //After 1second hide and show stuff
             toggleElementsWithStatus('stimulus');
             buildGrid(current_grid.level, current_grid.trial);
           }, 1000);
-          jsPsych.pluginAPI.setTimeout(function () {
+          this.jsPsych.pluginAPI.setTimeout(function () {
             //After the Trial Duration has passed.
             clearGrid();
           }, trial.stimulous_duration * 1000); //Show fixation for
@@ -579,7 +579,7 @@ var jsPsychVPT = (function (jspsych) {
         // clear the display
         display_element.innerHTML = "";
         // end trial
-        jsPsych.finishTrial(trial_data);
+        this.jsPsych.finishTrial(trial_data);
       }
     }
   }
